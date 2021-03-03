19b9587455a94b95aa3798bebc9d5f6292650c3d
LorenzoMarinucci <lorenmarinucci@gmail.com>
2021-03-03 20:55:43 -0300

    Service added

---
+++ src/main/java/guru/springframework/jokesapp/services/ChuckNorrisJokesGenerator.java
@@ -0,0 +1,23 @@
+package guru.springframework.jokesapp.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+@Service
+public class ChuckNorrisJokesGenerator implements JokesGenerator {
+
+	private ChuckNorrisQuotes generator;
+	
+	@Autowired
+	public ChuckNorrisJokesGenerator(ChuckNorrisQuotes generator) {
+		this.generator = generator;
+	}
+	
+	@Override
+	public String getJoke() {
+		return generator.getRandomQuote();
+	}
+
+}
---
+++ src/main/java/guru/springframework/jokesapp/services/JokesGenerator.java
@@ -0,0 +1,7 @@
+package guru.springframework.jokesapp.services;
+
+public interface JokesGenerator {
+	
+	public String getJoke();
+
+}
